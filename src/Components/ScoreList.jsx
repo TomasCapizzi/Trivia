dc673a6693beca24040c23a61ee05b5cac76b08b
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-24 16:07:00 -0300

    Addd hooks functions

--- src/Components/ScoreList.jsx
+++ src/Components/ScoreList.jsx
@@ -1,5 +1,6 @@
-import React,{useState, useEffect} from "react";
-import { database } from "../Firebase/users";
+import React,{useEffect, useState} from "react";
+
+import useGetScores from "../Hooks/useGetScores";
 
 export default function ScoreList({actualUser}){
 
@@ -7,35 +8,27 @@ export default function ScoreList({actualUser}){
     const [userScore, setUserScore] = useState([])
     const [handler, setHandler] = useState(false)
 
-    async function getScoreList(){
-        const scoreData = await database.collection('scores');
-        scoreData.get().then((query)=> setScores(query.docs.map((doc)=>{
-            return {...doc.data()}
-        })))        
-        setHandler(true);
-        getUserScores()
-    }
+    const {getScoreList} = useGetScores({setScores, setHandler, getUserScores})
 
     function getUserScores(){
         setUserScore(scores.filter(item => item.user === actualUser))
-
+        console.log(scores.filter(item => item.user === actualUser));
     }
 
     useEffect(()=>{
         getScoreList();
-        handler && console.log(scores.filter(item => item.user === actualUser));
-                 // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[handler])
 
     return(
         <div className='score-list'>
-            <p>scores</p>
-            { handler?
+            <p>Scores</p>
+            { userScore.length ?
                 userScore.map(
                     item => <p key={item.date}>{item.score}</p>
                 )
                 :
-                <></>
+                null
             }
         </div>
     )
