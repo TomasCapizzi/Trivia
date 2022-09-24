06b863348724b72e09e51171b6fef36ecfd54b1a
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-24 16:06:01 -0300

    Update Question.jsx

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Results from './Results';
 
-
 export default function Question({question , changeQuestion, incrementScore, score, listOfQuestions}){
 
     return(
